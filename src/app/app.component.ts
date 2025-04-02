e98c152ebfabd4faae0bf4adaced2c0bcb236975
Kodex-Baba <122707428+Kodex-Baba@users.noreply.github.com>
2025-04-02 16:06:55 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '101477407-lab-test2-comp3133';
+}
